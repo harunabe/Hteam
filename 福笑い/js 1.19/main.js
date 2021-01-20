e367b266d5daa027cf356f865b0146ad472d29aa
y20517 <y20517@iit.iwate-it.ac.jp>
2021-01-20 15:26:44 +0900

    Hteam
    福笑いができるhtmlです。

---
+++ 福笑い/js 1.19/main.js
@@ -0,0 +1,46 @@
+window.onload = function(){
+document.getElementsByClassName("result").text="";
+}
+//目
+document.getElementById("box11").onclick = function(){    
+    document.getElementById("demo1").innerHTML = '<img src="透過済み顔パーツ/目1.PNG" alt="サンプル画像">';
+    }
+document.getElementById("box12").onclick = function(){
+    document.getElementById("demo1").innerHTML = '<img src="透過済み顔パーツ/目2.PNG" alt="サンプル画像">';
+    }
+document.getElementById("box13").onclick = function(){
+    document.getElementById("demo1").innerHTML = '<img src="透過済み顔パーツ/目3.PNG" alt="サンプル画像">';
+    }
+
+//口
+document.getElementById("box21").onclick = function(){
+    document.getElementById("demo3").innerHTML = '<img src="透過済み顔パーツ/口1.PNG" alt="サンプル画像">';
+    }
+document.getElementById("box22").onclick = function(){
+    document.getElementById("demo3").innerHTML = '<img src="透過済み顔パーツ/口2.PNG" alt="サンプル画像">';
+    }
+document.getElementById("box23").onclick = function(){
+    document.getElementById("demo3").innerHTML = '<img src="透過済み顔パーツ/口3.PNG" alt="サンプル画像">';
+    }
+
+//鼻
+document.getElementById("box31").onclick = function(){
+    document.getElementById("demo2").innerHTML = '<img src="透過済み顔パーツ/鼻1.PNG" alt="サンプル画像">';
+    }
+document.getElementById("box32").onclick = function(){
+    document.getElementById("demo2").innerHTML = '<img src="透過済み顔パーツ/鼻2.PNG" alt="サンプル画像">';
+    }
+document.getElementById("box33").onclick = function(){
+    document.getElementById("demo2").innerHTML = '<img src="透過済み顔パーツ/鼻3.PNG" alt="サンプル画像">';
+    }
+
+//髪
+document.getElementById("box41").onclick = function(){
+    document.getElementById("demo0").innerHTML = '<img src="透過済み顔パーツ/髪1.PNG" alt="サンプル画像">';
+    }
+document.getElementById("box42").onclick = function(){
+    document.getElementById("demo0").innerHTML = '<img src="透過済み顔パーツ/髪2.PNG" alt="サンプル画像">';
+    }
+document.getElementById("box43").onclick = function(){
+    document.getElementById("demo0").innerHTML = '<img src="透過済み顔パーツ/髪3.PNG" alt="サンプル画像">';
+    }
